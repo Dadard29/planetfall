2d8a91fe1fbff6cd607a0d302cb48227f9376d1a
florian.charpentier <florian.charpentier@epita.fr>
2022-04-24 15:45:53 +0200

    fixed duplicated error message

--- planetfall/response.go
+++ planetfall/response.go
@@ -9,17 +9,21 @@ import (
 	"cloud.google.com/go/errorreporting"
 )
 
+// Behavior to use when an error in encountered in a http handler
+// Returns the message as a response to the request performer
+// Log the message, the error, and the request using the Error Reporting tool from GCloud
 func (s *Server) InternalServerError(w http.ResponseWriter, r *http.Request, err error, message string) {
 	err = fmt.Errorf("%s: %v", message, err)
 	s.errorReporting.Report(errorreporting.Entry{
 		Error: err,
 		Req:   r,
 	})
-	log.Printf("%s: %v", message, err)
+	log.Println(err)
 
 	http.Error(w, message, http.StatusInternalServerError)
 }
 
+// Returns an object as a JSON response
 func (s *Server) JsonResponse(w http.ResponseWriter, r *http.Request, v interface{}) {
 
 	out, err := json.Marshal(&v)
